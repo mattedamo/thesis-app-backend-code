3b918a6053f75683ddcfb844c4fe43dc23bbb98d
mattedamo <55213300+mattedamo@users.noreply.github.com>
2021-01-23 16:05:24 +0100

    Update validation-input-file.py

--- validation-input-file.py
+++ validation-input-file.py
@@ -6,10 +6,7 @@ def main():
     if "branch" not in input.keys():
         raise Exception("You have to insert the correct namespace manually into the input yaml file, "+ 
                         "to confirm that you have made the actual changes in it!")
-        
-    if input["branch"] != (os.environ["GITHUB_REF"].split("/")[-1] || os.environ["GITHUB_REF"].split("/")[-2]+"/"+os.environ["GITHUB_REF"].split("/")[-1]) :
-        raise Exception("You have to insert the correct namespace manually into the input yaml file, "+ 
-                        "to confirm that you have made the actual changes in it!")
+       
     if "clusters" not in input.keys():
         raise Exception("You have to specific the cluster name(s) where you want to deploy the application!")
 if __name__ == '__main__':
